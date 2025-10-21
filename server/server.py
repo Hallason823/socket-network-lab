f9f87330f40bede2d810789439bbc2fcba7f85cb
Hallason Matias <122497324+Hallason823@users.noreply.github.com>
2025-10-21 04:07:54 +0000

    feat: add user's nickname, command who and broadcast message

--- server/server.py
+++ server/server.py
@@ -2,13 +2,15 @@ import socket
 import threading
 
 class Server:
-    def __init__(self, port, ip, header, selected_format, disconnect_message):
+    def __init__(self, port, ip, header, selected_format, who_message, disconnect_message):
         self.port = port
         self.ip = ip
         self.header = header
         self.selected_format = selected_format
+        self.who_message = who_message
         self.disconnect_message = disconnect_message
-        self.active_clients = []
+        self.active_clients = {}
+        self.lock = threading.Lock()
         self.create_and_bind_socket()
         self.start_socket()
 
@@ -18,22 +20,82 @@ class Server:
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind(self.addr)
 
-    def handle_client(self, conn, addr):
+    def receive_message(self, conn):
+        try:
+            msg_length = conn.recv(self.header).decode(self.selected_format)
+            if not msg_length:
+                return None
+            msg_length = int(msg_length)
+            return conn.recv(msg_length).decode(self.selected_format)
+        except:
+            return None
+
+    def send_message(self, conn, msg):
+        msg = msg.encode(self.selected_format)
+        msg_header = str(len(msg)).encode(self.selected_format)
+        msg_header += b' ' * (self.header - len(msg_header))
+        conn.send(msg_header + msg)
+    
+    def register_nickname(self, conn, addr):
+        nickname = self.receive_message(conn)
+        if nickname is None:
+            conn.close()
+            return None
+        with self.lock:
+            if nickname in self.active_clients:
+                self.send_message(conn, "FAILED: nickname in used")
+                conn.close()
+                return None
+            else:
+                self.active_clients[nickname] = conn
+                print(f"[NEW USER] {nickname} joined from {addr}")
+                self.send_message(conn, f"User {nickname} joined")
+                return nickname
+
+    def send_active_users(self, conn):
+        with self.lock:
+            users = ", ".join(self.active_clients.keys())
+        self.send_message(conn, f"Connected users: {users}")
+
+    def broadcast_message(self, nickname, msg):
+        with self.lock:
+            for user, user_conn in self.active_clients.items():
+                if user != nickname:
+                    self.send_message(user_conn, f"FROM {nickname} [all]: {msg}")
+
+    def client_loop(self, conn, nickname):
         connected = True
-        print(f"[NEW CONNECTION] {addr} connected.")
         while connected:
-            msg_length = int(conn.recv(self.header).decode(self.selected_format))
-            msg = conn.recv(msg_length).decode(self.selected_format)
-            if msg == self.disconnect_message:
+            msg = self.receive_message(conn)
+            if msg is None or msg.upper() == self.disconnect_message:
                 connected = False
-            print(f"[{addr}] {msg}")
+                break
+            elif msg.upper() == "WHO":
+                self.send_active_users(conn)
+            else:
+                self.broadcast_message(nickname, msg)
+            print(f"[{nickname}] {msg}")
+
+    def disconnect_client(self, conn, nickname):
+        with self.lock:
+            if nickname in self.active_clients:
+                del self.active_clients[nickname]
         conn.close()
+        print(f"[DISCONNECTED] {nickname} disconnected.")
+
+    def handle_client(self, conn, addr):
+        nickname = self.register_nickname(conn, addr)
+        if nickname is None:
+            return
+        self.client_loop(conn, nickname)
+        self.disconnect_client(conn, nickname)
 
     def start_socket(self):
         print("[STARTING] server is starting...")
         self.server_socket.listen()
         while True:
             conn, addr = self.server_socket.accept()
-            thread = threading.thread(target=self.handle_client, args=(conn, addr))
+            self.send_message(conn, "Send your nickname to register, please.")
+            thread = threading.Thread(target=self.handle_client, args=(conn, addr))
             thread.start()
             print(f"[ACTIVE CONNECTIONS] {threading.active_count()-1}")
--- shared/config.py
+++ shared/config.py
@@ -1,8 +1,8 @@
 import random
 import socket
 
-PARAMETERS_NAME = ['--server-port', '--server-ip', '--header', '--selected-format', '--disconnect_message'] 
-DEFAULT_VALUES = [random.randint(1024, 49151), socket.gethostbyname(socket.gethostname()), 64, 'utf-8', "!DISCONNECT"]
+PARAMETERS_NAME = ['--server-port', '--server-ip', '--header', '--selected-format', '--who_message', '--disconnect_message'] 
+DEFAULT_VALUES = [random.randint(1024, 49151), socket.gethostbyname(socket.gethostname()), 64, 'utf-8', "!WHO", "!QUIT"]
 TYPES = ['int', 'string', 'int', 'string', 'string']
 
 def print_help():
