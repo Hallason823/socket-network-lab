9de1fe74dfcb9abab9e392c9aace1729e8a95ff7
Hallason Matias <122497324+Hallason823@users.noreply.github.com>
2025-10-21 07:49:46 +0000

    feat: add help text

--- shared/config.py
+++ shared/config.py
@@ -4,9 +4,26 @@ import socket
 PARAMETERS_NAME = ['--mode', '--server-port', '--server-ip', '--header', '--selected-format', '--who_message', '--disconnect_message'] 
 DEFAULT_VALUES = ['server', random.randint(1024, 49151), socket.gethostbyname(socket.gethostname()), 64, 'utf-8', "!WHO", "!QUIT"]
 TYPES = ['string', 'int', 'string', 'int'] + ['string']*3
+HELP_TEXT = """
+Usage: python main.py [OPTIONS]
+
+Options:
+  --mode=server|client        Choose to run as server or client (default: server)
+  --server-port=PORT          Set the port number for the server/client (default: random between 1024-49151)
+  --server-ip=IP              Set the IP address for the server/client (default: local machine IP)
+  --header=NUM                Number of bytes to represent message length (default: 64)
+  --selected-format=FORMAT    Encoding format for messages (default: utf-8)
+  --who_message=COMMAND       Command to list connected users (default: !WHO)
+  --disconnect_message=COMMAND Command to disconnect (default: !QUIT)
+  -h, --help                 Show this help message and exit
+
+Examples:
+  python main.py --mode=server --server-port=5050
+  python main.py --mode=client --server-ip=192.168.0.10 --server-port=5050
+"""
 
 def print_help():
-    print('\nUsage: python main.py [OPTIONS]\n')
+    print(HELP_TEXT)
 
 def print_error(error):
     if error == True:
